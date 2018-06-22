a71b5ffa5c7caaffb876c881ac7673cd31a70db4
Bob Covell <rcovell@rolet.com>
2018-06-22 11:34:28 -0500

    Move primary navigation to component

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,55 +1,14 @@
 <template>
   <div>
-    <div class="container">
-      <div class="row">
-        <div class="top-right links">
-          <router-link :to="{ name: 'home' }" class="nav-link" active-class>Home</router-link> |
-          <router-link :to="{ name: 'about' }" class="nav-link" active-class>About</router-link> |
-          <span v-if="authenticated">
-            <router-link :to="{ name: 'dashboard' }" class="nav-link" active-class>Dashboard</router-link> |
-            <router-link :to="{ name: 'settings.profile' }" :class="{'router-link-active': subIsActive('/settings')}">Settings</router-link> |
-            <a @click.prevent="logout" class=""  href="logout">
-              Logout
-            </a>
-          </span>
-          <!-- dropdown-item pl-3 -->
-          <span v-else>
-            <router-link :to="{ name: 'login' }">
-              Login
-            </router-link>
-            <!-- <router-link :to="{ name: 'register' }">
-              {{ $t('register') }}
-            </router-link> -->
-          </span>
-        </div>
-      </div>
-    </div>
-    <!-- <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="top-right links">
-            <router-link :to="{ name: 'home' }">Home</router-link> |
-            <router-link :to="{ name: 'about' }">About</router-link> |
-            <span v-if="authenticated && user">
-              <router-link :to="{ name: 'dashboard' }">Dashboard</router-link> |
-              <a @click.prevent="logout" class="dropdown-item pl-3"  href="logout">
-                Logout
-              </a>
-            </span>
-            <span v-else>
-              <router-link :to="{ name: 'login' }">Login</router-link>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
+    <primary-nav :authenticated="authenticated"></primary-nav>
     <div class="container">
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
+  import PrimaryNav from './PrimaryNav.vue';
+
   export default {
     data() {
       return {
@@ -67,6 +26,9 @@
         this.user = {}
       });
     },
+    components: {
+      'primary-nav': PrimaryNav
+    },
     methods: {
       async logout () {
         try {
@@ -78,12 +40,6 @@
           console.log(error.response.data.message)
         }
       },
-      subIsActive(input) {
-        const paths = Array.isArray(input) ? input : [input]
-        return paths.some(path => {
-          return this.$route.path.indexOf(path) === 0 // current path starts with this path string
-        })
-      }
     }
   }
 </script>
---
+++ resources/assets/js/components/PrimaryNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="top-right links">
+          <router-link :to="{ name: 'home' }" class="nav-link" active-class>Home</router-link> |
+          <router-link :to="{ name: 'about' }" class="nav-link" active-class>About</router-link> |
+          <span v-if="authenticated">
+            <router-link :to="{ name: 'dashboard' }" class="nav-link" active-class>Dashboard</router-link> |
+            <router-link :to="{ name: 'settings.profile' }" :class="{'router-link-active': subIsActive('/settings')}">Settings</router-link> |
+            <a @click.prevent="logout" class=""  href="logout">
+              Logout
+            </a>
+          </span>
+          <span v-else>
+            <router-link :to="{ name: 'login' }">
+              Login
+            </router-link>
+            <!-- <router-link :to="{ name: 'register' }">
+              {{ $t('register') }}
+            </router-link> -->
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      'authenticated': {
+        type: Boolean,
+        default: false,
+      },
+    },
+    mounted() {
+      //
+    },
+    methods: {
+      subIsActive(input) {
+        const paths = Array.isArray(input) ? input : [input]
+        return paths.some(path => {
+          return this.$route.path.indexOf(path) === 0
+        })
+      }
+    }
+  }
+</script>

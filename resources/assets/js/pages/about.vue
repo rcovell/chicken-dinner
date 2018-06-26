5a02c6bf2c8a16bd3ba20e538d72714f82c94c37
Bob Covell <rcovell@rolet.com>
2018-06-26 12:51:29 -0500

    Default content

--- resources/assets/js/pages/about.vue
+++ resources/assets/js/pages/about.vue
@@ -3,7 +3,18 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12">
-          About
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec quam molestie, gravida risus eu, efficitur tortor. Mauris fringilla mollis vestibulum. Donec sollicitudin finibus enim non ultrices. Nullam condimentum libero non semper posuere. Curabitur imperdiet nunc ipsum, a maximus sapien porta sit amet. Duis posuere consectetur dolor id pellentesque. Sed massa nisi, semper bibendum lacus et, dignissim consectetur risus. Mauris diam tellus, dictum ut tortor id, semper sodales dolor. Donec scelerisque tempor nisl, vel sodales dui cursus nec.
+          </p>
+
+          <p>
+            Nam ultricies lorem nulla, ac condimentum magna venenatis commodo. Duis sed eleifend ipsum. Donec et nisl sit amet nisi ultricies consectetur. Vestibulum nec ligula auctor orci hendrerit cursus. Donec diam urna, sagittis sit amet lacus sed, auctor blandit arcu. Suspendisse id ullamcorper risus. Proin non lacus blandit, viverra enim sed, pharetra urna.
+          </p>
+
+          <p>
+            Cras eleifend mauris at tempor egestas. Duis ligula est, feugiat ac laoreet sed, posuere et quam. Donec nec massa vel nulla fringilla pretium et eget massa. Aenean vel mauris nec tellus vulputate hendrerit eget ut risus. Vivamus vel orci eget nisi dapibus dapibus in sit amet ex. Mauris facilisis libero ut est gravida, vel lacinia nibh placerat. Pellentesque sit amet mauris nec orci venenatis tincidunt. Vestibulum a tortor ac metus consequat venenatis. Donec dictum ex lectus. Nulla sed elit et mi tincidunt interdum. Nam vitae turpis tellus. In sit amet dui eget velit elementum gravida id sit amet metus. Integer vitae mi id dolor pellentesque blandit at non ex.
+          </p>
         </div>
       </div>
       <router-view></router-view>
--- resources/assets/js/pages/dashboard.vue
+++ resources/assets/js/pages/dashboard.vue
@@ -3,7 +3,18 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12">
-          Dashboard
+          <h2>Dashboard</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ante pulvinar, aliquet mauris sed, fringilla neque. Nam ac pulvinar erat. Sed semper mauris nec congue efficitur. Suspendisse vel tristique libero. Nulla id nisl nunc. Mauris eget auctor eros. Sed est urna, sodales sit amet ipsum in, iaculis posuere ante.
+          </p>
+
+          <p>
+            Cras at est a erat malesuada hendrerit ac at nulla. In hac habitasse platea dictumst. Aliquam augue diam, posuere non sem id, imperdiet condimentum mi. In sed justo nec ante blandit dignissim. Fusce dignissim cursus elit at ornare. Mauris ultricies interdum leo a pharetra. Aenean et condimentum magna. Sed porta metus vel velit tincidunt viverra.
+          </p>
+
+          <p>
+            Nam quis ipsum diam. Duis volutpat pellentesque nibh, vitae maximus elit molestie non. Nam non purus odio. Suspendisse potenti. In sodales, tortor non porta porttitor, augue purus varius ipsum, ac maximus lorem risus vitae neque. Integer eu dui tellus. Ut ornare leo eu nulla consequat, malesuada eleifend ipsum aliquam. Integer blandit dapibus eros. Donec eget lobortis enim, non bibendum odio. Suspendisse sollicitudin ante nec mattis euismod. Nam faucibus mauris id viverra accumsan. Nunc ultricies, tellus vitae efficitur scelerisque, ex nisl fermentum nulla, luctus tincidunt tortor sapien id orci. Nam laoreet ipsum tellus, eget finibus turpis porttitor volutpat.
+          </p>
         </div>
       </div>
       <router-view></router-view>
